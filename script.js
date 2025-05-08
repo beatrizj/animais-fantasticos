1e5d4c345586b2db505f23e8dc71c0054d4e3b9b
Beatriz Januário <bibi_januario@hotmail.com>
2025-05-08 19:58:56 -0300

    scroll animation

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function initTabNav() {
 }
 initTabNav();
 
+//FAQ animation
 function initAccordion () {
   const accordionList = document.querySelectorAll(".js-accordion dt")
   const activeClass = "ativo"
@@ -40,4 +41,48 @@ function initAccordion () {
     })
   }
 }
-initAccordion()
+initAccordion();
+
+//Smooth scroll
+function initSmoothScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  }
+
+  linksInternos.forEach((item) => {
+    item.addEventListener("click", scrollToSection);
+  })
+}
+initSmoothScroll();
+
+//Scroll animation
+function initScrollAnimation() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6; //pega o valor de 60% da tela do usuário
+
+    function scrollAnimation() {
+      sections.forEach((item) => {
+        const sectionTop = item.getBoundingClientRect().top; //verifica o valor do topo do elemento
+        const isSectionVisible = (sectionTop - windowHalf) < 0;
+        if (isSectionVisible) {
+          item.classList.add("ativo");
+        }
+      })
+    }
+
+    window.addEventListener("scroll", scrollAnimation);
+
+    scrollAnimation()
+  }
+}
+initScrollAnimation();
